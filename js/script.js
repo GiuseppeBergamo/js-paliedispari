a3b2238516fd5e17ca5499957c27c0a4d751af8b
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-06-29 22:34:23 +0200

    fine esercisio pari e dispari

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
-function getRandomNumber(min, max) {
 
+const userChoice = prompt("Scegli pari o dispari");
+const userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+
+
+function getRandomNumber(min = 1, max = 5) {
+    randomNumber = Math.floor(Math.random() * (max + 1 - min)) + min;
+    return randomNumber;
 }
+
+
+const computerNumber = getRandomNumber()
+console.log(getRandomNumber);
+
+
+const resultNumber = getRandomNumber() + userNumber;
+console.log(resultNumber)
+
+
+function isEven(resultNumber) {
+    let result = false;
+
+    if (resultNumber % 2 === 0) {
+        result = true;
+    }
+    return result;
+}
+
+const verifica = isEven(resultNumber);
+
+if (userChoice === "pari" && verifica === true) {
+    console.log("Hai vinto!");
+} else if (userChoice === "dispari" && verifica === false) {
+    console.log("hai vinto!");
+} else console.log("hai perso");
